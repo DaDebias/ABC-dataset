af078a114dd5bdb22e142b37ac8fcf4a944e175d
thearol <thearollesloth@hotmail.com>
2023-03-01 16:07:43 +0100

    added f1 coref eval

--- experiments/coref_scripts/eval_da_coref.py
+++ experiments/coref_scripts/eval_da_coref.py
@@ -1,6 +1,9 @@
 import json
 import argparse
 import os
+from sklearn.metrics import classification_report, f1_score
+import numpy as np
+from collections import Counter
 
 # loads predictions in triplets
 def chunks(lst, n):
@@ -17,7 +20,63 @@ def get_data(filename):
     return preds
 
 def get_f1(chunk_preds):
-    pass
+    ref = []
+    male = []
+    fem = []
+    
+    labels_ref = list(np.repeat(1, len(chunk_preds)))
+    labels_anti_ref = list(np.repeat(0, len(chunk_preds)))
+
+    for p in chunk_preds:
+        ref_pred = p[0]
+        male_pred = p[1]
+        fem_pred = p[2]
+
+        cluster_ref = ref_pred['clusters']
+        cluster_male = male_pred['clusters']
+        cluster_fem = fem_pred['clusters']
+        
+        clusters = [cluster_ref, cluster_male, cluster_fem]   
+
+        for i, cluster in enumerate(clusters):
+            if i == 0:
+                #print("CLUSTER", cluster)
+                if cluster != []:
+                    ref.append(1)
+                else:
+                    ref.append(0)
+                
+            elif i==1:
+                if cluster != []:
+                    male.append(1)
+                else:
+                    male.append(0)
+
+            elif i==2:
+
+                if cluster != []:
+                    fem.append(1)
+                else:
+                    fem.append(0)
+                    
+    #f1_ref = f1_score(labels_ref, ref, average='macro', zero_division=1)
+    #f1_male = f1_score(labels_anti_ref, male, average='macro', zero_division=1)
+    #f1_fem = f1_score(labels_anti_ref, fem, average='macro', zero_division=1)
+
+    clf_report_ref = classification_report(labels_ref, ref, zero_division=1)
+    clf_report_anti_male = classification_report(labels_anti_ref, male, zero_division=1)
+    clf_report_anti_fem = classification_report(labels_anti_ref, fem, zero_division=1)
+    
+    print(f'''
+    Classification report for the reflexive case:
+    {clf_report_ref}
+    
+    Classification report for anti-reflexive male:
+    {clf_report_anti_male}
+    
+    Classification report for anti-reflexive female:
+    {clf_report_anti_fem}
+    ''')
 
 def get_percentage(chunk_preds):
 
@@ -48,22 +107,25 @@ def get_percentage(chunk_preds):
 
                 if cluster != []:
                     fem+=1
-    print()
-    print(f"TOTAL NUMBER OF CLUSTERS PREDICTED BY THE MODEL OUT OF {len(chunk_preds)} SENTENCES:")
-    print("Reflexive case", ref)
-    print("Anti-reflexive - MALE: ", male)
-    print("Anti-reflexive - FEMALE: ", fem)
-    print()
-    print("-----------------------------")
-    print()
-    print("PERCENTAGE OF PREDICTED CLUSTERS:")
-    print()
-    print("Correctly predicted clusters in the reflexive cases:")
-    print("reflexive: ", (ref/len(chunk_preds))*100, "% of the sentences are correctly coreferenced")
-    print()
-    print("Wrongly predicted clusters in the anti-reflexive cases:")
-    print("anti-reflexive possessive pronoun - MALE: ", ((male/len(chunk_preds))*100), "% of the sentences are wrongly coreferenced")
-    print("anti-reflexive possessive pronoun - FEMALE: ", ((fem/len(chunk_preds))*100), "% of the sentences are wrongly coreferenced")
+    print(
+
+        f''' TOTAL NUMBER OF CLUSTERS PREDICTED BY THE MODEL OUT OF {len(chunk_preds)} SENTENCES:
+        Reflexive case: {ref}
+        Anti-reflexive - MALE: {male}
+        Anti-reflexive - FEMALE: {fem}
+
+        -----------------------------
+
+        PERCENTAGE OF PREDICTED CLUSTERS:
+
+        Correctly predicted clusters in the reflexive cases:
+        reflexive: {(ref/len(chunk_preds))*100} % of the sentences are correctly coreferenced
+
+        Wrongly predicted clusters in the anti-reflexive cases:
+        anti-reflexive possessive pronoun - MALE: {((male/len(chunk_preds))*100)} % of the sentences are wrongly coreferenced
+        anti-reflexive possessive pronoun - FEMALE: {((fem/len(chunk_preds))*100)} % of the sentences are wrongly coreferenced
+        '''
+        )
 
 def main(output_type, coref_output_file):
     #get current working directory
